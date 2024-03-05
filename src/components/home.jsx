428efa22710e26f4085e6675b63cedc7f29f9434
Lagzen Thakuri <thakurizen2@gmail.com>
2024-03-06 00:44:31 +0545

    banner responsive

--- src/components/home.jsx
+++ src/components/home.jsx
@@ -14,8 +14,10 @@ export default function Home(){
     <div className='flex justify-center items-center bg-primary w-full h-screen snap-center'>
       <Link to="/" className="absolute top-4 left-5 text-5xl text-grey font-serif">LT</Link>
       
-      <h1 className='font-serif text-5xl text-white  text-center'>Lagzen Thakuri  </h1>
-      <p className='absolute sm:bottom-64 bottom-72 text-grey'>Mern Stack Developer</p>
+      <h1 className='font-serif text-5xl text-white text-center'>Lagzen Thakuri <br/>
+       <span className=' font-thin text-2xl  text-grey '>Mern Stack Developer</span> 
+       </h1>
+       
       <Link to="/poetry" className="absolute right-2 bottom-2 text-white border rounded-full p-2 hover:bg-white hover:text-black  font-serif text-xl">under construction</Link>
 
     <div className="absolute bottom-2 left-2 ">
